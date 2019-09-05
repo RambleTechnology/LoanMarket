8b5ba5edd3ddb072d1037bdafc24d016b73b3cc1
huomeisu2580 <1416497812@qq.com>
2019-09-05 21:53:30 +0800

    banner图片修改新增黑科技内容

---
+++ LoanMarket/Controllers/BannerDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoanMarket.Controllers
+{
+    public class BannerDetailController : Controller
+    {
+        public ActionResult BuildwoMillion()
+        {
+            return View ();
+        }
+        public ActionResult MembershipModel()
+        {
+            return View();
+        }
+        public ActionResult OverseasWithdrawal()
+        {
+            return View();
+        }
+        
+    }
+}
+ 
