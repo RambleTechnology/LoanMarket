c9dfa5493d8b8f829b2586d5a40935ff7d221245
huomeisu2580 <1416497812@qq.com>
2019-09-06 15:01:53 +0800

    hebing

--- LoanMarket/obj/Release/Package/PackageTmp/Scripts/Common/common.js
+++ LoanMarket/obj/Release/Package/PackageTmp/Scripts/Common/common.js
@@ -1,6 +1,10 @@
-﻿var rootPath = "http://47.104.186.251/Content/Files/";
+﻿
+var rootPath = "http://47.104.186.251/Content/Files/";
+// 产品二维码
+var qrCodeRootPath = "http://localhost:52829/Content/Images/QrCode/";
+//var qrCodeRootPath = "http://47.104.186.251/Content/Images/QrCode/";
 
 var api = {
-    productList: "/Api/ProductType", 
-    productTypeList:"/Api/ProductType/",
+    productList: "/Api/ProductType",
+    productTypeList: "/Api/ProductType/",
 };
